b7ac6604324d5ee153def4e77b67c5f1d503f0be
koopa31 <clement.cazorla31@gmail.com>
2022-06-15 16:35:11 +0200

    ajout différents réseaux light weght

--- src/napari_svetlana/CNN2D.py
+++ src/napari_svetlana/CNN2D.py
@@ -3,31 +3,30 @@ from torch.nn import Sequential, Conv2d, ReLU, MaxPool2d, AdaptiveAvgPool2d, Lin
 
 
 class CNN2D(Module):
-    def __init__(self, labels_number, channels_nb):
+    def __init__(self, labels_number, channels_nb, depth, kersize):
         super(CNN2D, self).__init__()
 
-        kersize = 3
         nconv = 16
         npool = 2
-        self.cnn_layers = Sequential(
-            # Defining a 2D convolution layer
-            Conv2d(channels_nb, nconv, kernel_size=(kersize, kersize), stride=(1, 1), padding=(1, 1)),
-            ReLU(inplace=True),
-            BatchNorm2d(nconv),
-            #Conv2d(nconv, nconv, kernel_size=(kersize, kersize), stride=(1, 1), padding=(1, 1)),
-            #ReLU(inplace=True),
-            #BatchNorm2d(nconv),
-            MaxPool2d(kernel_size=npool, stride=npool),
-            # Defining a 2D convolution layer
-            Conv2d(nconv, nconv, kernel_size=(kersize, kersize), stride=(1, 1), padding=(1, 1)),
-            ReLU(inplace=True),
-            BatchNorm2d(nconv),
-            MaxPool2d(kernel_size=npool, stride=npool),
-            # Defining a 2D convolution layer
-            #Conv2d(nconv, nconv, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
-            #ReLU(inplace=True),
-            #MaxPool2d(kernel_size=2, stride=2),
-        )
+        layers_list = []
+
+        for i in range(depth):
+            if i == 0:
+                # Defining a 2D convolution layer
+                layers_list.append(Conv2d(channels_nb, nconv, kernel_size=(kersize, kersize), stride=(1, 1),
+                                          padding=(1, 1)))
+                layers_list.append(ReLU(inplace=True))
+                layers_list.append(BatchNorm2d(nconv))
+                layers_list.append(MaxPool2d(kernel_size=npool, stride=npool))
+            else:
+                # Defining a 2D convolution layer
+                layers_list.append(Conv2d(nconv, nconv, kernel_size=(kersize, kersize), stride=(1, 1),
+                                          padding=(1, 1)))
+                layers_list.append(ReLU(inplace=True))
+                layers_list.append(BatchNorm2d(nconv))
+                layers_list.append(MaxPool2d(kernel_size=npool, stride=npool))
+
+        self.cnn_layers = Sequential(*layers_list)
         self.avg_pool = Sequential(AdaptiveAvgPool2d(output_size=(1, 1)))
         self.fc = Sequential(Linear(nconv, labels_number + 1))
 
@@ -35,7 +34,6 @@ class CNN2D(Module):
     def forward(self, x):
 
         x = self.cnn_layers(x)
-        #x = x.view(x.size(0), -1)
         x = self.avg_pool(x)
         x = torch.squeeze(x)
         x = self.fc(x)
